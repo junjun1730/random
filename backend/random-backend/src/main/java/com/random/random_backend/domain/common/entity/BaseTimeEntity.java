5af2f63c2391bd59ac10d94aef59cedeeabffa2e
leeJS <dlwnstjzld@gmail.com>
2026-05-25 06:35:13 +0900

    add auth domain

--- backend/random-backend/src/main/java/com/random/random_backend/domain/common/entity/BaseTimeEntity.java
+++ backend/random-backend/src/main/java/com/random/random_backend/domain/common/entity/BaseTimeEntity.java
@@ -1,4 +1,4 @@
-package com.random.random_backend.common.entity;
+package com.random.random_backend.domain.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
